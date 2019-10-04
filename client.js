c2c9933098891929c806af9145c227e1496002ab
Grant Krieger <fgkrieger13@gmail.com>
2019-10-04 18:24:56 -0500

    adds input display to the table

--- client.js
+++ client.js
@@ -4,4 +4,54 @@ $(document).ready(readyNow);
 
 function readyNow(){
     console.log('JQ');
+    $('#submitButton').on('click', salaryFunction);
+    $('table').on('click', '.delete', deleteFunction);
+}
+let monthlySalary = 0;
+var employees = [];
+function salaryFunction(){
+    let nameFirst = $('#firstName').val();
+    let nameLast = $('#lastName').val();
+    let idNum = $('#identity').val();
+    let title = $('#title').val();
+    let annSalary = $('#annSalary').val();
+
+    let employeeInfo= {
+        empFirst: nameFirst,
+        empLast: nameLast,
+        empId: idNum,
+        empTitle: title,
+        empSalary: annSalary
+    };
+    employees.push(employeeInfo);
+    console.log(employees)
+
+    $('table').append(`
+    <tr>
+            <td>`+ (nameFirst) +`</td>
+            <td>`+ (nameLast) +`</td>
+            <td>`+ (idNum) +`</td>
+            <td>`+ (title) +`</td>
+            <td>`+ (annSalary) +`</td>
+            <td class = 'delete'><button>Delete</button></td>
+     </tr>`)
+     $('#firstName').val('');
+     $('#lastName').val('');
+     $('#identity').val('');
+     $('#title').val('');
+     $('#annSalary').val('');
+         
+     
+}
+
+
+
+function deleteFunction(){
+    console.log('delete');
+    $(this).closest('tr').remove();
+    
+    for (let index = 0; index < employees.length; index++) {
+        console.log(employees[index])
+       
+   }
 }
